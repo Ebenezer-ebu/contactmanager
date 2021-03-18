c0b41b6da6356116a3f2a0ed06770820f926beb2
Ebenezer-ebu <eifezulike@gmail.com>
2021-03-18 15:14:40 +0100

    initial commit

---
+++ src/components/Test/Test.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+
+class Test extends Component {
+
+    state = {
+        title: '',
+        body: ''
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/posts/1")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    title: data.title,
+                    body: data.body
+                })
+            });
+    }
+
+    // componentWillMount() {
+    //     console.log('Component will mount...')
+    // }
+
+    // componentDidUpdate() {
+    //     console.log('Component did update...')
+    // }
+
+    // componentWillUpdate() {
+    //     console.log('Component will update...')
+    // }
+
+    // componentWillReceiveProps(nextProps, nextState) {
+    //     console.log('Component will receive')
+    // }
+
+    // static getDerivedStateFromProps(nextProps, prevState) {
+    //     return null
+    // }
+
+    // getSnapshotBeforeUpdate(prevProps, prevState) {
+    //     console.log('get snapshot before update')
+    // }
+     
+    render() {
+        const { title, body } = this.state;
+        return (
+            <div>
+                <h1>{title}</h1>
+                <p>{body}</p>
+            </div>
+        )
+    }
+}
+
+export default Test
